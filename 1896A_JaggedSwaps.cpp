956ef8118281fb587ee8b6c7bc7572abd907afc5
Karan Soni <karansoni7973@gmail.com>
2025-01-29 10:33:37 +0530

    1899A,1896A

---
+++ 1896A_JaggedSwaps.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        vector<int> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+
+        if(arr[0] != 1){
+            cout<<"NO"<<endl;
+        }
+        else{
+            cout<<"YES"<<endl;
+        }
+    }
+
+    return 0;
+}
